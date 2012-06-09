4b34cff3e46ab26987468cf9e0787691aa781533
Hiroshi Araya <new747@gmail.com>
2012-06-09 16:33:07 +0900

    triggerd every 15min

--- ifttt.js
+++ ifttt.js
@@ -1,46 +1,79 @@
 var maxMessageCount = 100;
+var threshold = 80;
+var qryCondition = '!(in:trash) label:ifttt-tw';
+var triggerCondition = '!(in:trash) label:ifttt-trigger';
+var triggerHourOnWeekDays = [7, 11];
+var triggerMinuteOnWeekDays = 45;
+
+function getCollectTriggerTitle(collectThreads) {
+	if (collectThreads.length > threshold) {
+		return "閾";
+	}
+
+	var triggerThreads = GmailApp.search(triggerCondition);
+	if (triggerThreads.length) {
+		moveToTrash(triggerThreads);
+		return "文";
+	}
+	var dd = new Date();
+	if (dd.getDay() == 0 || dd.getDay() == 6) {
+		return "";
+	}
+	if (dd.getMinutes() < triggerMinuteOnWeekDays) {
+		return "";
+	}
+	for (var i = 0; i < triggerHourOnWeekDays.length; i++) {
+		if (triggerHourOnWeekDays[i] == dd.getHours()) {
+			return "時";
+		}
+	}
+	return "";
+}
+
 function ifttt() {
-	var qryCondition = '!(in:trash) label:ifttt-tw';
-	var messages = collectMessages(qryCondition);
+	var messages = collectMessages();
 	if (messages) {
 		messages.send();
 		messages.moveToTrash();
 	}
 }
 
-function collectMessages(qryCondition) {
+function moveToTrash(movableArray) {
+	for (var j = 0; j < movableArray.length; j++) {
+		movableArray[j].moveToTrash();
+	}
+}
+
+function collectMessages() {
 	var msgs = []; 
 	var threads = GmailApp.search(qryCondition, 0, maxMessageCount).reverse();
-	Logger.log(threads.length);
-	for (var i = 0; i < Math.min(maxMessageCount, threads.length); i++) {
+	var triggerTitle = getCollectTriggerTitle(threads);
+
+	Logger.log(triggerTitle + ":" + threads.length);
+	if (!triggerTitle.length) {
+		return null;
+	}
+
+	for (var i = 0; i < threads.length; i++) {
 		msgs = msgs.concat(threads[i].getMessages());
 	}
-	Logger.log("collected" + msgs.length);
 	
 	return msgs.length ? {
 		send: function() {
 			GmailApp.sendEmail(
 				msgs[0].getTo(),
-				"【gas】" + msgs.length + "件 at " + new Date(),
+				"【gas】" + msgs.length + "件:" + triggerTitle + "@" + new Date(),
 				(function() {
 					var sendMessageBody = "";
 					for (var j = 0; j < msgs.length; j++) {
 						sendMessageBody += msgs[j].getBody().replace(/<[^>]*>/g, "");
-						sendMessageBody += "--------------\n";
-						if (!((j + 1) % 20)) {
-							Logger.log("body created:" + j);
-						}
+						sendMessageBody += "----\n";
 					}
 					return sendMessageBody;
 				})());
 		},
 		moveToTrash: function() {
-			for (var j = 0; j < msgs.length; j++) {
-				msgs[j].moveToTrash();
-				if (!((j + 1) % 20)) {
-					Logger.log("move to trashed:" + j);
-				}
-			}
+			moveToTrash(msgs);
 		}
 	} : null;
 }
